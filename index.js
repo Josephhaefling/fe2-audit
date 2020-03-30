46be5081d23013df00ec099652fce61df917f2d1
Joe Haefling <josephhaefling@gmail.com>
2020-03-30 10:08:57 -0600

    This is what I got done

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const audit = {
 
     // What is the value of `this` when we call fn()?
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'global window object';
 
     return result;
   },
@@ -31,7 +31,7 @@ const audit = {
 
     // What is the value of `this` when we call data.verify())?
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'data';
 
     return result;
   },
@@ -40,7 +40,7 @@ const audit = {
 
 
 
-  
+
 
 
   exerciseC() {
@@ -54,7 +54,7 @@ const audit = {
 
     // What is the value of `this` when we call dog.bark();
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'global window object';
 
     return result;
   },
@@ -93,7 +93,13 @@ const audit = {
     greetingGenerator();
 
     const result = [{
-      // 'ADD YOUR RESULT HERE';
+      'D': 'welcome'
+    }, {
+      'A': 'hi'
+    }, {
+      'B': 'hi'
+    }, {
+      'C': 'hi'
     }];
 
     return result;
@@ -109,7 +115,7 @@ const audit = {
   exerciseE() {
 
     var shoe = 'heel';
-    // Log A: 
+    // Log A:
 
     function putOnShoe() {
       shoe = 'boot';
@@ -123,8 +129,14 @@ const audit = {
     // Log D: shoe
 
     const result = [{
-      // 'ADD YOUR RESULT HERE';
-    }];
+        'A': 'heel'
+      }, {
+        'C': 'heel'
+      }, {
+        'B': 'boot'
+      }, {
+        'D': 'boot'
+      }];
 
     return result;
   },
@@ -174,7 +186,9 @@ const audit = {
     // e.g.
     // ['Beckon', 'El Five', 'ChoLon', 'Super Mega Bien']
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = const restaurantNames = () => {
+      return restaurants.map(restaurant => restaurant.name)
+    };
 
     return result;
   },
@@ -220,7 +234,7 @@ const audit = {
     ];
 
     // Return an array of objects that include just the name of the restaurant and whether takeout is available
-    
+
     // e.g.
     // [
     //    { restaurant: 'Beckon', takeOut: false },
@@ -228,7 +242,10 @@ const audit = {
     //    ..etc
     // ]
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = restaurants.reduce((takeoutList, restaurant) => {
+      takeoutList[restaurant.name] = restaurant.takeout
+      return takeoutList
+    }, []);;
 
     return result;
   },
@@ -289,7 +306,7 @@ const audit = {
 
   exerciseBonus() {
 
-    // There is a war and nobody knows - the alphabet war! 
+    // There is a war and nobody knows - the alphabet war!
     // There are two groups of hostile letters. The tension between left side letters and right side letters was too high and the war began.
 
     // Write a function that accepts fight string consists of only small letters and return who wins the fight. When the left side wins return 'Left side wins!', when the right side wins return 'Right side wins!', in other case return 'Let's fight again!'.
@@ -329,7 +346,7 @@ const audit = {
       four: alphabetWar('wwwwwwz')
     };
 
-    return result;    
+    return result;
   }
 };
 
